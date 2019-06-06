15eeaabbb9ad82873651d9708d6cae4180fcd98e
Jonathan Willian <jonathan.willian.tod@gmail.com>
2019-06-06 12:46:37 -0300

    Added a little workaround to make lists not logout user on production.

--- src/Middlewares/user.js
+++ src/Middlewares/user.js
@@ -1,28 +1,29 @@
 const fetch = require('node-fetch');
 
-const endpoint = process.env.MAGENTO_ENDPOINT;
-const url = `${endpoint}/users/me`;
+// const endpoint = process.env.MAGENTO_ENDPOINT;
+// const url = `${endpoint}/users/me`;
 const urlCustomer = 'https://staging.zaxapp.com.br/graphql';
+const urlCustomerProd = 'https://www.zaxapp.com.br/graphql';
 const getToken = ({ headers = {} }) => headers.authorization;
 
-const getUserByToken = async (Authorization) => {
-  try {
-    const response = await fetch(url, {
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization,
-      },
-    });
-    const { id } = await response.json();
-
-    return { id };
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.log(err);
-    return {};
-  }
-};
+// const getUserByToken = async (Authorization) => {
+//   try {
+//     const response = await fetch(url, {
+//       method: 'get',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization,
+//       },
+//     });
+//     const { id } = await response.json();
+//
+//     return { id };
+//   } catch (err) {
+//     // eslint-disable-next-line no-console
+//     console.log(err);
+//     return {};
+//   }
+// };
 
 const getCustomerByToken = async (Authorization) => {
   try {
@@ -46,18 +47,46 @@ const getCustomerByToken = async (Authorization) => {
   }
 };
 
+const getCustomerByTokenProd = async (Authorization) => {
+  try {
+    const response = await fetch(urlCustomerProd, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization,
+      },
+      body: '{"query":"{customer{id}}"}',
+    });
+    const { data = {} } = await response.json();
+    const { customer } = data;
+    const { id } = customer;
+
+    return { id };
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+    return {};
+  }
+};
+
 const userMiddleware = async (req, res, next) => {
   const token = getToken(req);
-  const user = await getUserByToken(token);
+  // const user = await getUserByToken(token);
   const customer = await getCustomerByToken(token);
+  const customerProd = await getCustomerByTokenProd(token);
 
-  if (user.id !== undefined) {
-    req.userId = user.id;
-    return next();
-  }
+  // if (user.id !== undefined) {
+  //   req.userId = user.id;
+  //   return next();
+  // }
 
   if (customer.id !== undefined) {
     req.userId = customer.id;
+    return next();
+  }
+
+  if (customerProd.id !== undefined) {
+    req.userId = customerProd.id;
     return next();
   }
 
